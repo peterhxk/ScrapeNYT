86a9b53faf6e75d2ae6cbf5d7673d5b1596b976f
Peter Huang <peterhxk@gmail.com>
2025-07-25 13:21:01 -0400

    Changed to use Newspaper4k for extracting full articles

--- TestScrapeContent.py
+++ TestScrapeContent.py
@@ -1,15 +1,26 @@
 from newspaper import Article
 
 url = 'https://www.nytimes.com/2025/07/20/obituaries/edwin-feulner-dead.html'
+article = Article(url)
 
-try:
-    article = Article(url)
-    article.download()
-    article.parse()
-    print("Title:", article.title)
-    print("Authors:", article.authors)
-    print("Publish Date:", article.publish_date)
-    print("Top Image:", article.top_image)
-    print("Text:\n", article.text)  # Print first 1000 characters
-except Exception as e:
-    print(f"Failed to extract article: {e}")
+article.download()
+article.parse()
+
+print(article.title)
+print(article.text)
+
+
+# try:
+#     article = Article(url, language='en')
+#     article.download()
+#     article.parse()
+#     article.nlp()
+#     print("Summary:", article.summary)
+#     print("keywords:", article.keywords)
+#     # print("Title:", article.title)
+#     # print("Authors:", article.authors)
+#     # print("Publish Date:", article.publish_date)
+#     # print("Top Image:", article.top_image)
+#     # print("Text:\n", article.text)
+# except Exception as e:
+#     print(f"Failed to extract article: {e}")
